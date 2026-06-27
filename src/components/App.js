c5c10dca4cdb8cc6f7260f44f5eed43d298ae065
kayalvizhi <madhukayal88@gmail.com>
2026-06-27 20:44:11 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,28 @@
+import React from 'react';
+import Tooltip from './Tooltip';
 
-import React from "react";
-import './../styles/App.css';
-
-const App = () => {
+function App() {
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div id="main" style={{ padding: '5px', textAlign: 'center' }}>
+      <h2>React Composition: Tooltip Demo</h2>
+      
+      <div style={{ margin: '50px 0' }}>
+        {/* Wrapping a button element with a tooltip */}
+        <Tooltip text="This is a top tooltip!">
+          <button style={{ padding: '10px 20px' }}>Hover over me</button>
+        </Tooltip>
+      </div>
+
+      <div style={{ margin: '50px 0' }}>
+        {/* Wrapping a text paragraph element with a tooltip */}
+        <Tooltip text="Secondary Info text!">
+          <span style={{ borderBottom: '1px dashed #333', cursor: 'pointer' }}>
+            Hover here instead
+          </span>
+        </Tooltip>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
